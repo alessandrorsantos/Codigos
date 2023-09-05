184a04358e72853ea79a1286a32f21cf9df1a8a6
alessandrorsantos <81659122+alessandrorsantos@users.noreply.github.com>
2023-09-05 08:47:28 -0300

    Add files via upload

---
+++ digite dois numeros mostra se é par ou impar.py
@@ -0,0 +1,16 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+
+if numero1 % 2 == 0:
+    print(f"{numero1} é um número par.")
+else:
+    print(f"{numero1} é um número ímpar.")
+
+
+if numero2 % 2 == 0:
+    print(f"{numero2} é um número par.")
+else:
+    print(f"{numero2} é um número ímpar.")
+
+
